1298993a5746823d58768eb3a7d75dfe41979a53
Z_Doctor <13635485+Z-Doctor@users.noreply.github.com>
2018-09-21 00:18:08 -0700

    change format

--- src/main/java/zdoctor/zcoremod/asm/patches/EntityLivingPatch.java
+++ src/main/java/zdoctor/zcoremod/asm/patches/EntityLivingPatch.java
@@ -62,36 +62,28 @@ public class EntityLivingPatch extends ClassTransformer {
 		List<FieldNode> fields = classNode.fields;
 
 		String sig = "L" + DataParameter.getClassName() + "<Ljava/lang/Float;>;";
-		FieldNode lastHealthField = new FieldNode(ASMConstants.PSF, "LAST_HEALTH", DataParameter.getDescriptor(), sig,
-				null);
+		FieldNode lastHealthField = new FieldNode(ASMConstants.PSF, "LAST_HEALTH", DataParameter.getDescriptor(), sig, null);
 		fields.add(lastHealthField);
 
 		sig = "L" + DataParameter.getClassName() + "<" + NBTTagCompound.getDescriptor() + ">;";
-		FieldNode potionWatcher = new FieldNode(ASMConstants.PSF, "POTION_WATCHER", DataParameter.getDescriptor(), sig,
-				null);
+		FieldNode potionWatcher = new FieldNode(ASMConstants.PSF, "POTION_WATCHER", DataParameter.getDescriptor(), sig, null);
 		fields.add(potionWatcher);
 	}
 
 	public void clinit(MethodNode method) {
-//		System.out.println("Initialized: LAST_HEALTH");
-
 		InsnBuilder list = new InsnBuilder();
 		// Add a DataParameter named LAST_HEALTH to Entity
 		list.ldc(Type.getType(EntityLivingBase.getDescriptor()));
-		list.getStatic(DataSerializers.getClassName(), DataSerializers.field_187193_c.getName(),
-				DataSerializer.getDescriptor());
-		list.invokeStatic(EntityDataManager.getClassName(), EntityDataManager.func_187226_a.getName(),
-				EntityDataManager.func_187226_a.getDescriptor(), false);
+		list.getStatic(DataSerializers.getClassName(), DataSerializers.field_187193_c.getName(), DataSerializer.getDescriptor());
+		list.invokeStatic(EntityDataManager.getClassName(), EntityDataManager.func_187226_a.getName(), EntityDataManager.func_187226_a.getDescriptor(), false);
 
 		list.dup();
 		list.putStatic(EntityLivingBase.getClassName(), "LAST_HEALTH", DataParameter.getDescriptor());
 		list.putStatic(ZPackage + "/tweaks/EntityTweaks", "LAST_HEALTH", DataParameter.getDescriptor());
 
 		list.ldc(Type.getType(EntityLivingBase.getDescriptor()));
-		list.getStatic(DataSerializers.getClassName(), DataSerializers.field_192734_n.getName(),
-				DataSerializer.getDescriptor());
-		list.invokeStatic(EntityDataManager.getClassName(), EntityDataManager.func_187226_a.getName(),
-				EntityDataManager.func_187226_a.getDescriptor(), false);
+		list.getStatic(DataSerializers.getClassName(), DataSerializers.field_192734_n.getName(), DataSerializer.getDescriptor());
+		list.invokeStatic(EntityDataManager.getClassName(), EntityDataManager.func_187226_a.getName(), EntityDataManager.func_187226_a.getDescriptor(), false);
 
 		list.dup();
 		list.putStatic(EntityLivingBase.getClassName(), "POTION_WATCHER", DataParameter.getDescriptor());
@@ -101,26 +93,19 @@ public class EntityLivingPatch extends ClassTransformer {
 	}
 
 	public void entityInit(MethodNode method) {
-//		System.out.println("Injected in EntityLivingBase.entityInit: EntityTweaks.entityInit");
-
 		InsnBuilder list = new InsnBuilder();
 		list.var(Opcodes.ALOAD);
-		list.getField(EntityLivingBase.getClassName(), Entity.field_70180_af.getName(),
-				EntityDataManager.getDescriptor());
-		list.invokeStatic(ZPackage + "/tweaks/EntityTweaks", "entityInit",
-				"(" + EntityDataManager.getDescriptor() + ")V", false);
+		list.getField(EntityLivingBase.getClassName(), Entity.field_70180_af.getName(), EntityDataManager.getDescriptor());
+		list.invokeStatic(ZPackage + "/tweaks/EntityTweaks", "entityInit", "(" + EntityDataManager.getDescriptor() + ")V", false);
 		list.addToEnd(method);
 	}
 
 	public void setHealth(MethodNode method) {
-//		System.out.println("Injecting code into: EntityLivingBase.setHealth");
-
 		InsnBuilder list = new InsnBuilder();
 		// Get Current Health
 		list.var(Opcodes.ALOAD);
 		list.dup();
-		list.invokeVirtual(EntityLivingBase.getClassName(), EntityLivingBase.func_110143_aJ.getName(),
-				EntityLivingBase.func_110143_aJ.getDescriptor(), false);
+		list.invokeVirtual(EntityLivingBase.getClassName(), EntityLivingBase.func_110143_aJ.getName(), EntityLivingBase.func_110143_aJ.getDescriptor(), false);
 		list.invokeVirtual(EntityLivingBase.getClassName(), "setLastHealth", "(F)V", false);
 
 		list.addToStart(method);
@@ -130,8 +115,7 @@ public class EntityLivingPatch extends ClassTransformer {
 		InsnBuilder list = new InsnBuilder();
 		list.var(Opcodes.ALOAD);
 		list.var(Opcodes.ALOAD, 1);
-		list.invokeStatic(ZPackage + "/tweaks/EntityTweaks", "readNbt",
-				"(" + EntityLivingBase.getDescriptor() + NBTTagCompound.getDescriptor() + ")V", false);
+		list.invokeStatic(ZPackage + "/tweaks/EntityTweaks", "readNbt", "(" + EntityLivingBase.getDescriptor() + NBTTagCompound.getDescriptor() + ")V", false);
 		list.addToEnd(method);
 
 	}
@@ -140,30 +124,25 @@ public class EntityLivingPatch extends ClassTransformer {
 		InsnBuilder list = new InsnBuilder();
 		list.var(Opcodes.ALOAD);
 		list.var(Opcodes.ALOAD, 1);
-		list.invokeStatic(ZPackage + "/tweaks/EntityTweaks", "writeNbt",
-				"(" + EntityLivingBase.getDescriptor() + NBTTagCompound.getDescriptor() + ")V", false);
+		list.invokeStatic(ZPackage + "/tweaks/EntityTweaks", "writeNbt", "(" + EntityLivingBase.getDescriptor() + NBTTagCompound.getDescriptor() + ")V", false);
 		list.addToEnd(method);
 
 	}
 
 	public void updatePotionMetadata(MethodNode method) {
-//		System.out.println("Injecting code into: EntityLivingBase.updatePotionMetadata");
-
 		for (AbstractInsnNode node : method.instructions.toArray()) {
 			if (node instanceof FieldInsnNode) {
 				FieldInsnNode fieldNode = (FieldInsnNode) node;
 				if (fieldNode.desc.equals("Ljava/util/Map;") && node.getNext() instanceof MethodInsnNode) {
 					MethodInsnNode methodNode = (MethodInsnNode) fieldNode.getNext();
-					if (methodNode.name.equals("values") && methodNode.desc.equals("()Ljava/util/Collection;")
-							&& methodNode.getNext() instanceof VarInsnNode) {
+					if (methodNode.name.equals("values") && methodNode.desc.equals("()Ljava/util/Collection;") && methodNode.getNext() instanceof VarInsnNode) {
 						VarInsnNode collection = (VarInsnNode) methodNode.getNext();
 						int var = collection.var;
 						InsnBuilder list = new InsnBuilder();
-//						list.print("Updating Potions");
 						list.var(Opcodes.ALOAD);
 						list.var(Opcodes.ALOAD, var);
-						list.invokeStatic(ZPackage + "/tweaks/EntityTweaks", "updatePotionMetadata",
-								"(" + EntityLivingBase.getDescriptor() + "Ljava/util/Collection;" + ")V", false);
+						list.invokeStatic(ZPackage + "/tweaks/EntityTweaks", "updatePotionMetadata", "(" + EntityLivingBase.getDescriptor() + "Ljava/util/Collection;" + ")V",
+								false);
 						list.addAt(method, collection);
 						break;
 					}
@@ -173,41 +152,31 @@ public class EntityLivingPatch extends ClassTransformer {
 	}
 
 	public void resetPotionEffectMetadata(MethodNode method) {
-//		System.out.println("Injecting code into: EntityLivingBase.resetPotionEffectMetadata");
-
 		InsnBuilder list = new InsnBuilder();
 
 		list.var(Opcodes.ALOAD);
-		list.invokeStatic(ZPackage + "/tweaks/EntityTweaks", "resetPotionEffectMetadata",
-				"(" + EntityLivingBase.getDescriptor() + ")V", false);
+		list.invokeStatic(ZPackage + "/tweaks/EntityTweaks", "resetPotionEffectMetadata", "(" + EntityLivingBase.getDescriptor() + ")V", false);
 
 		list.addTo(method);
 	}
 
 	public void notifyDataManagerChange(MethodNode method) {
-//		System.out.println("Injecting code into: EntityLivingBase.notifyDataManagerChange");
-
 		InsnBuilder list = new InsnBuilder();
 		list.var(Opcodes.ALOAD);
 		list.var(Opcodes.ALOAD, 1);
-		list.invokeStatic(ZPackage + "/tweaks/EntityTweaks", "notifyDataManagerChange",
-				"(" + EntityLivingBase.getDescriptor() + DataParameter.getDescriptor() + ")V", false);
+		list.invokeStatic(ZPackage + "/tweaks/EntityTweaks", "notifyDataManagerChange", "(" + EntityLivingBase.getDescriptor() + DataParameter.getDescriptor() + ")V", false);
 
 		list.addTo(method);
 	}
 
 	public void addGetLastHealth(ClassNode classNode) {
-//		System.out.println("Added Method: addGetLastHealth");
-
 		MethodNode getLastHealth = new MethodNode(Opcodes.ACC_PUBLIC, "getLastHealth", "()F", null, null);
 		InsnBuilder list = new InsnBuilder();
 
 		list.var(Opcodes.ALOAD);
-		list.getField(EntityLivingBase.getClassName(), Entity.field_70180_af.getName(),
-				EntityDataManager.getDescriptor());
+		list.getField(EntityLivingBase.getClassName(), Entity.field_70180_af.getName(), EntityDataManager.getDescriptor());
 		list.getStatic(EntityLivingBase.getClassName(), "LAST_HEALTH", DataParameter.getDescriptor());
-		list.invokeVirtual(EntityDataManager.getClassName(), EntityDataManager.func_187225_a.getName(),
-				EntityDataManager.func_187225_a.getDescriptor(), false);
+		list.invokeVirtual(EntityDataManager.getClassName(), EntityDataManager.func_187225_a.getName(), EntityDataManager.func_187225_a.getDescriptor(), false);
 		list.checkCastFloat();
 		list.floatValue();
 
@@ -218,19 +187,15 @@ public class EntityLivingPatch extends ClassTransformer {
 	}
 
 	public void addSetLastHealth(ClassNode classNode) {
-//		System.out.println("Added Method: setLastHealth");
-
 		MethodNode getLastHealth = new MethodNode(Opcodes.ACC_PUBLIC, "setLastHealth", "(F)V", null, null);
 		InsnBuilder list = new InsnBuilder();
 
 		list.var(Opcodes.ALOAD);
-		list.getField(EntityLivingBase.getClassName(), Entity.field_70180_af.getName(),
-				EntityDataManager.getDescriptor());
+		list.getField(EntityLivingBase.getClassName(), Entity.field_70180_af.getName(), EntityDataManager.getDescriptor());
 		list.getStatic(EntityLivingBase.getClassName(), "LAST_HEALTH", DataParameter.getDescriptor());
 		list.var(Opcodes.FLOAD, 1);
 		list.valueOfFloat();
-		list.invokeVirtual(EntityDataManager.getClassName(), EntityDataManager.func_187227_b.getName(),
-				EntityDataManager.func_187227_b.getDescriptor(), false);
+		list.invokeVirtual(EntityDataManager.getClassName(), EntityDataManager.func_187227_b.getName(), EntityDataManager.func_187227_b.getDescriptor(), false);
 		list.end();
 		list.addTo(getLastHealth);
 
